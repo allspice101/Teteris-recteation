878e6f887b1a402a70865edc55def2fe43ccb093
allspice101 <60833082+allspice101@users.noreply.github.com>
2021-03-08 13:51:46 -0700

    Update teterisgame.cpp

--- .teteris_recreation/teterisgame.cpp
+++ .teteris_recreation/teterisgame.cpp
@@ -13,6 +13,16 @@ int lastlevel = 0;
 #define tTile()
 #define lineTile()
 int main() {
+	int width, height, bpp;
+	uint8_t* rgb_image = stbi_load("tile_l.png", &width, &height, &bpp, 3);
+	uint8_t* rgb_image = stbi_load("tile_l_reverse.png", &width, &height, &bpp, 3);
+	uint8_t* rgb_image = stbi_load("tile_line.png", &width, &height, &bpp, 3);
+	uint8_t* rgb_image = stbi_load("tile_square.png", &width, &height, &bpp, 3);
+	uint8_t* rgb_image = stbi_load("tile_squiggle.png", &width, &height, &bpp, 3);
+	uint8_t* rgb_image = stbi_load("tile_squiggle_reverse.png", &width, &height, &bpp, 3);
+	uint8_t* rgb_image = stbi_load("tile_t.png", &width, &height, &bpp, 3);
+	stbi_image_free(rgb_image);
+	return 0;
 	ofstream pref;
 	ofstream save;
 	pref.open("prefrences.cpp");
